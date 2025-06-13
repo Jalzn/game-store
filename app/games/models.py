423bc5646a15cffcd915dbbdc5b49d8b55549933
Jalzn <jalmirfsjr@gmail.com>
2025-06-13 19:33:23 -0300

    Fix missing table name for games

--- app/games/models.py
+++ app/games/models.py
@@ -2,6 +2,8 @@ from sqlmodel import SQLModel, Field
 
 
 class Game(SQLModel, table=True):
+    __tablename__ = "games"
+
     id: int = Field(default=None, primary_key=True)
     title: str
     genre: str
